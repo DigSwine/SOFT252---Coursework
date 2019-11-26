0186f57d1c4ba54d85174994385c8ac8cb25ae39
(s) Morgan Wilson-Slider <morgan.wilson-slider@students.plymouth.ac.uk>
2019-11-26 20:41:14 +0000

    Data into Txt

--- Soft252-cw/src/soft252/cw/Soft252Cw.java
+++ Soft252-cw/src/soft252/cw/Soft252Cw.java
@@ -6,80 +6,57 @@ import soft252.cw.GUI.GUI_Home;
 import soft252.cw.Classes.Appointments;
 import soft252.cw.Classes.Clinic;
 import soft252.cw.Classes.Perscriptions;
-
+        
+//Data Imports
+import java.io.*;
+import java.io.File; 
+import java.io.FileNotFoundException; 
+import java.util.Scanner; 
+        
+        
 public class Soft252Cw 
 {
-    public static void main(String[] args) 
+
+    public static void main(String[] args) throws IOException 
     {
-      SetAll();
+        // Set Defult Data
+        String content = 
+            "D, KimBrook, 15, Shorsberry Avanue, Plymouth, PL3 3TD / "
+            + "D, Drew, Valintine, 10, Shortway Road, Plymouth, PL1 1WL / "
+            + "D, Bob, Ross, 1, Golden Street, Plymouth, PL4 4DX / "
+            + "P, David, Charger, Male, 54, 99, Long Road, Plymouth, PL5 2RT, DCharger, 12345 / "
+            + "P, Kimberly, Little, Female, 23, 12, Foxwood Road, Plymouth, PL2 4DZ, KLittle, 1212 / "
+            + "P, Luke, Walker, Male, 17, 23, Berry Road, Plymouth, PL1 2BR, LWalker, 123123 / "
+            + "A, Karran, Southbank, 29, Hallow Way, Plymouth, PL8 9KL / "
+            + "S, Shallisa, Beonce, 100, Deo Lane, Plymouth, PL3 2DO / "
+            + "C, MadeUp Pharmacy, 50, Long Road, Plymouth, PL5 2RT / "
+            + "CD, Paracetomol, 1.50, Over The Counter - Drug, 12 Tablets, Releaves Pain, 100 / "
+            + "CD, Ibuprofen, 1.25, Over The Counter - Drug, 12 Tablets, Releaves Pain, 100 / "
+            + "CD, Epipen, 5.00, Over The Counter  - Drug, 1 use, Emergancy Useage, 100 / "
+            + "CD, Norflex, 10.00, Perscription  - Drug, 26 Tablets, Muscle Relaxer, 100 / "
+            + "CD, Calcium Carbonate, 3.50, Over The Counter  - Drug, 20 Tablets, prevent or to treat a calcium deficiency, 100 / "
+            + "CD, Morphine, 12.00, Perscription  - Drug, 10 Patches,  Treats moderate to severe pain, 100 / "
+            + "CD, Beclomethasone Nasal, 5.50, Over The Counter  - Drug, Nasle Spray - 24 Sprays, prevent hayfeaver allergies, 100 / "
+            + "CD, Opium Tincture, 3.50, Perscription  - Drug, 5 Tablets, used to treat diarrhea, 100 / "
+            + "CD, Dextroamphetamine, 1.50, Perscription  - Drug, 20 Tablets,  used to treat narcolepsy and ADHD, 100 / "
+            + "CD, Baclofene, 3.50, Perscription  - Drug, 24 Tablets, muscle relaxer and an antispasmodic agent, 100 / "
+            + "AP, 0003, 0002, This patient is suffering from hayfever / "
+            + "PP, 0003, 0007, Beclomethasone Nasal, 1, 1 / ";
+      
+      
+        File file = new File("Data.txt");
+        FileWriter fw = new FileWriter(file.getAbsoluteFile());
+        BufferedWriter bw = new BufferedWriter(fw);
+        bw.write(content);
+        bw.close();
+
+         Scanner sc = new Scanner(file); 
+        sc.useDelimiter("\\Z"); 
+        System.out.println(sc.next());  
         
+        
+       // //Open GUI
         GUI_Home Home = new GUI_Home();
         Home.show();
-        
-    }   
-    
-    public static void SetAll()
-    {
-     //Setting Patients, Doctors, Admin, Secatery
-        Doctors Doc_one = new Doctors("Jess", "KimBrook", 15 , "Shorsberry Avanue", "Plymouth", "PL3 3TD");
-        Doctors Doc_two = new Doctors("Drew", "Valintine", 10, "Shortway Road", "Plymouth", "PL1 1WL");
-        Doctors Doc_three = new Doctors("Bob", "Ross", 1, "Golden Street", "Plymouth", "PL4 4DX");
-        Patients Pat_one = new Patients("David", "Charger", "Male", 54, 99, "Long Road", "Plymouth", "PL5 2RT", "DCharger", "12345");
-        Patients Pat_two = new Patients("Kimberly", "Little", "Female", 23, 12, "Foxwood Road", "Plymouth", "PL2 4DZ", "KLittle", "1212");
-        Patients Pat_three = new Patients("Luke", "Walker", "Male", 17, 23, "Berry Road", "Plymouth", "PL1 2BR", "LWalker", "123123");
-        Admin Adm_one = new Admin("Karran", "Southbank", 29, "Hallow Way", "Plymouth", "PL8 9KL");
-        Secretary Sec_one = new Secretary("Shallisa", "Beonce", 100, "Deo Lane", "Plymouth", "PL3 2DO");
-        Clinic Cli_one = new Clinic("MadeUp Pharmacy", 50, "Long Road", "Plymouth", "PL5 2RT");
-        Clinic Drug_one = new Clinic("Paracetomol", 1.50, "Over The Counter - Drug", "12 Tablets, Releaves Pain", 100);
-        Clinic Drug_two = new Clinic("Ibuprofen", 1.25, "Over The Counter - Drug", "12 Tablets, Releaves Pain", 100);
-        Clinic Drug_three = new Clinic("Epipen", 5.00, "Over The Counter  - Drug", "1 use, Emergancy Useage", 100);
-        Clinic Drug_four = new Clinic("Norflex", 10.00, "Perscription  - Drug", "26 Tablets, Muscle Relaxer", 100);
-        Clinic Drug_five = new Clinic("Calcium Carbonate", 3.50, "Over The Counter  - Drug", "20 Tablets, prevent or to treat a calcium deficiency", 100);
-        Clinic Drug_six = new Clinic("Morphine", 12.00, "Perscription  - Drug", "10 Patches,  Treats moderate to severe pain", 100);
-        Clinic Drug_seven = new Clinic("Beclomethasone Nasal", 5.50, "Over The Counter  - Drug", "Nasle Spray - 24 Sprays, prevent hayfeaver allergies", 100);
-        Clinic Drug_eight = new Clinic("Opium Tincture", 3.50, "Perscription  - Drug", "5 Tablets, used to treat diarrhea", 100);
-        Clinic Drug_nine = new Clinic("Dextroamphetamine", 1.50, "Perscription  - Drug", "20 Tablets,  used to treat narcolepsy and ADHD", 100);
-        Clinic Drug_ten = new Clinic("Baclofene", 3.50, "Perscription  - Drug", "24 Tablets, muscle relaxer and an antispasmodic agent.", 100);
-        
-        //SetAppointments
-        Appointments Appt_one = new Appointments(0003, 0002, "This patient is suffering from hayfever");
-        
-        
-        //SetPerscritions
-        Perscriptions Per_one = new Perscriptions(0003, 0007, "Beclomethasone Nasal", 1, 1);
-        
-//        System.out.println("Doctors");
-//        System.out.println(Doc_one.getDoctor_FirstName());        
-//        System.out.println(Doc_two.getDoctor_FirstName());
-//        System.out.println(Doc_three.getDoctor_FirstName());
-//        System.out.println("");
-//        System.out.println("Patients");
-//        System.out.println(Pat_one.getPatient_Firstname());
-//        System.out.println(Pat_two.getPatient_Firstname());
-//        System.out.println(Pat_three.getPatient_Firstname());
-//        System.out.println("");
-//        System.out.println("Admin");
-//        System.out.println(Adm_one.Admin_FirstName);
-//        System.out.println("");
-//        System.out.println("Secretary");
-//        System.out.println(Sec_one.getSecretary_FirstName());
-//        System.out.println("");
-//        System.out.println("Clinic");
-//        System.out.println(Cli_one.getClinic_Name());
-//        System.out.println("");
-//        System.out.println("Clinic Stock");
-//        System.out.println(Drug_one.getItem_Name() + " Quantity: " + Drug_one.getItem_Stock());
-//        System.out.println(Drug_two.getItem_Name() + " Quantity: " + Drug_two.getItem_Stock());
-//        System.out.println(Drug_three.getItem_Name() + " Quantity: " + Drug_three.getItem_Stock());
-//        System.out.println(Drug_four.getItem_Name() + " Quantity: " + Drug_four.getItem_Stock());
-//        System.out.println(Drug_five.getItem_Name() + " Quantity: " + Drug_five.getItem_Stock());
-//        System.out.println(Drug_six.getItem_Name() + " Quantity: " + Drug_six.getItem_Stock());
-//        System.out.println(Drug_seven.getItem_Name() + " Quantity: " + Drug_seven.getItem_Stock());
-//        System.out.println(Drug_eight.getItem_Name() + " Quantity: " + Drug_eight.getItem_Stock());
-//        System.out.println(Drug_nine.getItem_Name() + " Quantity: " + Drug_nine.getItem_Stock());
-//        System.out.println(Drug_ten.getItem_Name() + " Quantity: " + Drug_ten.getItem_Stock());
-//        System.out.println(Appt_one.getPatient_Notes());
-//        System.out.println(Per_one.getPerscription_Name());
-        
     }
 }
